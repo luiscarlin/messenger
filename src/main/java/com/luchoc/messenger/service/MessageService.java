f0c638e89ee92dcab686a25175091b34182a5fc0
luchoc <luiscarlin@gmail.com>
2015-11-08 00:06:55 -0500

    query parameters for pagination and filtering added

--- src/main/java/com/luchoc/messenger/service/MessageService.java
+++ src/main/java/com/luchoc/messenger/service/MessageService.java
@@ -1,6 +1,7 @@
 package com.luchoc.messenger.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -16,10 +17,36 @@ public class MessageService {
 		return new ArrayList<Message>(messages.values());
 	}
 	
+	public List<Message> getAllMessagesForYear(int year) { 
+		List<Message> messagesForYear = new ArrayList<>(); 
+		
+		Calendar cal = Calendar.getInstance(); 
+		
+		for (Message message : messages.values()) { 
+			cal.setTime(message.getCreated());
+			
+			if (cal.get(Calendar.YEAR) == year) { 
+				messagesForYear.add(message); 
+			}
+		}
+		return messagesForYear; 
+	}
+	
+	public List<Message> getAllMessagesPaginated(int start, int size) { 
+		List<Message> list = new ArrayList<Message>(messages.values()); 
+		
+		if (start + size > list.size())  { 
+			return list;
+		}
+		
+		return list.subList(start, start  + size); 
+	
+	}
+	
 	public Message getMessage(long id) { 
 		return messages.get(id); 
 	}
-	
+		
 	public Message addMessage(Message message) { 
 		message.setId(messages.size() + 1);
 		message.setCreated(new Date());
