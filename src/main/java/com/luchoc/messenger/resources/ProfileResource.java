06c09121270ce1b0cacec809936d00f9808f5a9f
luchoc <luiscarlin@gmail.com>
2015-11-07 23:35:46 -0500

    fixed profiles to use profile names instead of ids for identification

--- src/main/java/com/luchoc/messenger/resources/ProfileResource.java
+++ src/main/java/com/luchoc/messenger/resources/ProfileResource.java
@@ -33,21 +33,21 @@ public class ProfileResource {
 	}
 	
 	@PUT
-	@Path("/{profileId}")
-	public Profile updateProfile(@PathParam("profileId") long profileId, Profile profile) {
-		profile.setId(profileId);
+	@Path("/{profileName}")
+	public Profile updateProfile(@PathParam("profileName") String profileName, Profile profile) {
+		profile.setProfileName(profileName);
 		return profileService.updateProfile(profile);
 	}
 	
 	@DELETE
-	@Path("/{profileId}")
-	public Profile deleteProfile(@PathParam("profileId") long profileId) {
-		return profileService.removeProfile(profileId);
+	@Path("/{profileName}")
+	public Profile deleteProfile(@PathParam("profileName") String profileName) {
+		return profileService.removeProfile(profileName);
 	}
 	
 	@GET
-	@Path("/{profileId}")
-	public Profile getProfile(@PathParam("profileId") long profileId) { 
-		return profileService.getProfile(profileId);
+	@Path("/{profileName}")
+	public Profile getProfile(@PathParam("profileName") String profileName) { 
+		return profileService.getProfile(profileName);
 	}
 }
--- src/main/java/com/luchoc/messenger/service/ProfileService.java
+++ src/main/java/com/luchoc/messenger/service/ProfileService.java
@@ -10,32 +10,32 @@ import com.luchoc.messenger.model.Profile;
 
 public class ProfileService {
 	
-	private Map<Long, Profile> profiles = DatabaseClass.getProfiles(); 
+	private Map<String, Profile> profiles = DatabaseClass.getProfiles(); 
 	
 	public List<Profile> getAllProfiles() {
 		return new ArrayList<Profile>(profiles.values());
 	}
 	
-	public Profile getProfile(long id) { 
-		return profiles.get(id); 
+	public Profile getProfile(String profileName) { 
+		return profiles.get(profileName); 
 	}
 	
 	public Profile addProfile(Profile profile) { 
 		profile.setId(profiles.size() + 1);
 		profile.setCreated(new Date());
-		profiles.put(profile.getId(), profile);
+		profiles.put(profile.getProfileName(), profile);
 		return profile;
 	}
 	
 	public Profile updateProfile(Profile profile) { 
-		if (profile.getId() <= 0) { 
+		if (profile.getProfileName().isEmpty()) { 
 			return null; 
 		}
-		profiles.put(profile.getId(), profile); 
+		profiles.put(profile.getProfileName(), profile); 
 		return profile; 
 	}
 	
-	public Profile removeProfile(long id) { 
-		return profiles.remove(id); 
+	public Profile removeProfile(String profileName) { 
+		return profiles.remove(profileName); 
 	}
 }
