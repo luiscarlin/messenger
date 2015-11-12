983a3db6e5a418438e1ccf2552cfb1adda85439e
luchoc <luiscarlin@gmail.com>
2015-11-12 00:10:40 -0500

    Added subresource for comments

--- src/main/java/com/luchoc/messenger/model/Comment.java
+++ src/main/java/com/luchoc/messenger/model/Comment.java
@@ -1,25 +1,21 @@
 package com.luchoc.messenger.model;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
 @XmlRootElement
-public class Message {
+public class Comment {
 
 	private long id;
 	private Date created; 
 	private String author;
 	private String message; 
-	private Map<Long, Comment> comments = new HashMap<>();
 	
-	public Message() {
+	public Comment() {
 	}
 
-	public Message(long id, String author, String message) {
+	public Comment(long id, String author, String message) {
 		this.id = id;
 		this.author = author;
 		this.created = new Date();
@@ -56,14 +52,5 @@ public class Message {
 
 	public void setMessage(String message) {
 		this.message = message;
-	}
-	
-	@XmlTransient
-	public Map<Long, Comment> getComments() {
-		return comments;
-	}
-
-	public void setComments(Map<Long, Comment> comments) {
-		this.comments = comments;
 	}
 }
